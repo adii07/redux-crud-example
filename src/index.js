3b6c22bfeeeebd771cd08321c0b8182e912c2d74
adii07 <adityasharma5119@gmail.com>
2022-03-15 16:01:29 +0530

    redux slice

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {configureStore} from '@reduxjs/toolkit';
+import {Provider} from 'react-redux';
 
+const store=configureStore({
+  reducer:{
+    
+  },
+});
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );

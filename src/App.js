644f4547f66657a544031b47f828d342d8b8f0c8
adii07 <adityasharma5119@gmail.com>
2022-03-15 16:35:44 +0530

    dispatch hook

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import './App.css';
-import { useSelector } from 'react-redux';
+import { useSelector,useDispatch } from 'react-redux';
+import { addUser } from './features/Users';
 function App() {
-  const userList=useSelector((state)=>state.users.value)
+  const userList=useSelector((state)=>state.users.value);
+  const dispatch=useDispatch();
   return (
     <div className="App">
       <div className='addUser'>
         <input type="text" placeholder='Name'></input>
         <input type="text" placeholder='Username'></input>
-        <button>Add User</button>
+        <button onChange={(e)=>{}} >Add User</button>
       </div>
 
       <div className='displayUsers'>
+        {userList.map((user,id)=>{
+          return (
+            <div key={id}>
+              <h1 >{user.name}</h1>
+              <h1>{user.username}</h1>
+            </div>
 
+          )
+        })}
       </div>
     </div>
   );
